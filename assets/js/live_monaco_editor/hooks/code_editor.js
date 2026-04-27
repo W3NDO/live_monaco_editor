e7b18c8caefa979a6922ccd45812b58430fbf5ab
w3ndo <wendonyang@gmail.com>
2026-04-27 15:49:00 +0300

    changes

--- assets/js/live_monaco_editor/hooks/code_editor.js
+++ assets/js/live_monaco_editor/hooks/code_editor.js
@@ -16,6 +16,7 @@ const CodeEditorHook = {
       if (this.el.dataset.changeEvent && this.el.dataset.changeEvent !== "") {
         this.codeEditor.standalone_code_editor.onDidChangeModelContent(
           (event) => {
+            if (this.suppress) return
             if (this.el.dataset.target && this.el.dataset.target !== "") {
               this.pushEventTo(
                 this.el.dataset.target,
