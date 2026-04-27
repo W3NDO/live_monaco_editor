9d4023c76ba9f2de0efa95dbdab25a381d9abfc9
w3ndo <wendonyang@gmail.com>
2026-04-27 11:33:49 +0300

    changes

--- lib/live_monaco_editor.ex
+++ lib/live_monaco_editor.ex
@@ -202,9 +202,10 @@ defmodule LiveMonacoEditor do
   @doc """
   Apply the changes as edits.
   """
-  @spec apply_edits(Socket.t(), Map.t(), keyword()) :: Socket.t()
+  @spec apply_edits(Socket.t(), List.t(), keyword()) :: Socket.t()
   def apply_edits(socket, changes, opts \\ []) when is_list(changes) do
     to = Keyword.get(opts, :to, @default_path)
+    IO.inspect(changes, label: "Usere Anderungen ")
     push_event(socket, "lme:applyEdits:#{to}", changes)
   end
 end
